eb6cffbf8dae8ba59e3f2b68f490d0953bdf3c2e
gdavidev <gabrieldavid.net@gmail.com>
2023-11-28 00:52:16 -0300

    DB connection and welcome page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index']);
+ROute::get('/events/create', [EventController::class, 'create']);
+
+Route::get('/produtos/{id?}', function ($id = 1) {
+    $busca = request('search');
+
+    return view('product', ['id' => $id, 'busca' => $busca]);
 });
